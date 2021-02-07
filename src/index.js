ba72f4324bec7151e1dd643bebab3a6b36120eae
Christopher Tribbey <7040708+CTribbey@users.noreply.github.com>
2021-02-06 21:44:22 -0600

    Update list display
    Added bolding of current selected move in the history list

--- src/index.js
+++ src/index.js
@@ -104,6 +104,17 @@ class Game extends React.Component {
       const desc = move
         ? "Go to move #" + move + " :" + this.formatPosition(step.moveNumber)
         : "Go to game start";
+
+      if (move === this.state.stepNumber) {
+        return (
+          <li key={move}>
+            <button onClick={() => this.jumpTo(move)}>
+              <b>{desc}</b>
+            </button>
+          </li>
+        );
+      }
+
       return (
         <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
